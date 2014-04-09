bd71687e3c6e6269bb07fb57c175432fa51af54b
awatekiran <awatekiran@outlook.com>
2014-04-09 14:53:53 +0530

    Update inprogress.rb
    changed formatting

--- inprogress.rb
+++ inprogress.rb
@@ -32,79 +32,87 @@ $request = ''
 $domain = ''
 
 def api()
-$request = <<eof
-<packet version="1.6.5.0">
-<server>
-<get_protos/>
-</server>
-</packet>
+	$request = <<eof
+	<packet version="1.6.5.0">
+		<server>
+			<get_protos/>
+		</server>
+	</packet>
 eof
-return $request
+	return $request
 end
 
 def stat(domain)
-print "Enter domain name > "
-$domain = gets.chomp()
-$request = <<eof
-<packet version="1.6.5.0">
-<site>
-<get>
-<filter><name>#{$domain}</name></filter>
-<dataset>
-<gen_info/>
-</dataset>
-</get>
-</site>
-</packet>
+	print "Enter domain name > "
+	$domain = gets.chomp()
+	$request = <<eof
+	<packet version="1.6.5.0">
+		<site>
+			<get>
+				<filter>
+					<name>
+					#{$domain}
+					</name>
+				</filter>
+				<dataset>
+					<gen_info/>
+				</dataset>
+			</get>
+		</site>
+	</packet>
 eof
-return $domain
-return $request
+	return $domain
+	return $request
 end
 
 def suspend(domain)
-print "Enter domain to be suspended > "
-$domain = gets.chomp()
-$request = <<eof
-<packet version="1.6.5.0">
-<site>
-<set>
-<filter><name>#{$domain}</name></filter>
-<values>
-<gen_setup>
-<status>8</status>
-</gen_setup>
-</values>
-</set>
-</site>
-</packet>
+	print "Enter domain to be suspended > "
+	$domain = gets.chomp()
+	$request = <<eof
+	<packet version="1.6.5.0">
+		<site>
+			<set>
+				<filter>
+					<name>
+					#{$domain}
+					</name>
+				</filter>
+				<values>
+					<gen_setup>
+						<status>8</status>
+					</gen_setup>
+				</values>
+			</set>
+		</site>
+	</packet>
 eof
-return $domain
-return $request
+	return $domain
+	return $request
 end
 
 def get_response()
-puts "1. Check API
-2. Check domain stat
-3. Suspend the domain"
+	puts "1. Check API
+	2. Check domain stat
+	3. Suspend the domain"
 
-print "Enter your choice > "
-input = gets.chomp()
-c = Integer(input)
+	print "Enter your choice > "
+	input = gets.chomp()
+	c = Integer(input)
+	if c==1
+        api()
+        print $request
+	elsif c==2
+        stat($domain)
+        print $request
+	elsif c==3
+        suspend($domain)
+        puts $request
+	else
+        print "Wrong Choice !!"
+	end
 
-if c==1
-	api()
-	print $request
-elsif c==2
-	stat($domain)
-	print $request
-elsif c==3
-	suspend($domain)
-	puts $request
-else 
-	print "Wrong Choice !!"
-end	
-
-response = $client.request($request)
-puts response.body
+	response = $client.request($request)
+	puts response.body
 end
+
 get_response()
